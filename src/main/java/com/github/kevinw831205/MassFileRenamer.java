f19db629cd8be8fd937f5b83c208f2512508222d
KevinW <bird831205@hotmail.com>
2020-01-19 22:20:51 -0500

    defining some fields

---
+++ src/main/java/com/github/kevinw831205/MassFileRenamer.java
@@ -0,0 +1,19 @@
+package com.github.kevinw831205;
+
+public class MassFileRenamer {
+
+    String directory;
+    String baseFileName;
+    String[] extensions;
+
+    public MassFileRenamer(String directory, String baseFileName, String... extensions ) {
+        this.directory = directory;
+        this.baseFileName = baseFileName;
+        this.extensions = extensions;
+    }
+
+    public void renameFiles(){
+
+    }
+
+}
