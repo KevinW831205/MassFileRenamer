4c2e8467f0d8f58c0c594016bac6c9b60bb1af6e
KevinW <bird831205@hotmail.com>
2020-01-21 08:44:00 -0500

    referencing enum

--- src/main/java/com/github/kevinw831205/Main.java
+++ src/main/java/com/github/kevinw831205/Main.java
@@ -3,15 +3,18 @@ package com.github.kevinw831205;
 public class Main {
 
     public static void main(String[] args) {
-        String directory = "D:\\github-repo\\MassFileRenamer\\TestResource"; // directory of files to rename
-        String baseFileName = "html"; // base file name of files index ex: file1 -> file
-        String[] extensions = new String[]{".html"};
-
-        String newFileName = "newHtml";
-
-
+//        String directory = "D:\\github-repo\\MassFileRenamer\\TestResource"; // directory of files to rename
+//        String baseFileName = "html"; // base file name of files index ex: file1 -> file
+//        String[] extensions = new String[]{".html"};
+//
+//        String newFileName = "newHtml";
 
 
+        String directory = FileFields.DIRECTORY.getText();
+        String baseFileName = FileFields.BASEFILENAME.getText();
+        String[] extensions = FileFields.EXTENSIONS.getExtensions();
+
+        String newFileName = FileFields.NEWFILENAME.getText();
 
         MassFileRenamer renamer = new MassFileRenamer(directory, baseFileName, extensions);
 
